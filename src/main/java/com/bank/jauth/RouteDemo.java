2933c6cf4a0f79ba4549af6e6d6ff4488019e701
zoubi <moricio123@protonmail.com>
2019-03-15 12:32:54 +0100

    test add route v2

---
+++ src/main/java/com/bank/jauth/RouteDemo.java
@@ -0,0 +1,15 @@
+package com.bank.jauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/test")
+public class RouteDemo {
+
+    @GetMapping(path = "/", produces = "application/json")
+    public String getAll() {
+      return "true";
+    }
+}

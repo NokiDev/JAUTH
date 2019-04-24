f1d62482baa33b1b4068a173dc6c3c761a41d842
NokiDev <noki.dev@gmail.com>
2019-04-25 01:24:22 +0200

    Added Id Annotation for Token

--- src/main/java/com/bank/jauth/model/v1/Token.java
+++ src/main/java/com/bank/jauth/model/v1/Token.java
@@ -3,6 +3,7 @@ package com.bank.jauth.model.v1;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -11,6 +12,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Token {
+	@Id
 	@GeneratedValue
 	private String token;
 	@Column(nullable = false)
